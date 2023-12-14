be7a4547728359953f23414093fca5d39876aad8
chooli <chooli.yip@gmail.com>
2023-12-14 13:38:14 -0500

    migrate to spring boot 3.2 and junit 5

--- src/main/java/com/jumkid/usercentral/config/SecurityConfig.java
+++ src/main/java/com/jumkid/usercentral/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +23,9 @@ public class SecurityConfig {
     @Value("${jwt.token.enable}")
     private boolean enableTokenCheck;
 
+    @Value("${jwt.token.validate}")
+    private boolean enableTokenValidation;
+
     @Value("${jwt.token.introspect.url}")
     private String tokenIntrospectUrl;
 
@@ -33,24 +37,15 @@ public class SecurityConfig {
     }
 
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> {
-                try {
-                    auth
-                            .anyRequest().authenticated()
-                            .and()
-                            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                            .and()
-                            .addFilterBefore(new BearerTokenRequestFilter(enableTokenCheck, tokenIntrospectUrl, restTemplate),
-                                    UsernamePasswordAuthenticationFilter.class)
-                            .csrf().disable(); // enable this if the authorization service exposure to public
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    log.error("Failed to initiate security policies due to {}", e.getMessage());
-                }
-        });
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .addFilterBefore(new BearerTokenRequestFilter(enableTokenCheck, enableTokenValidation, tokenIntrospectUrl, restTemplate),
+                        UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
+        return httpSecurity.build();
     }
 
     @Bean
--- src/test/java/com/jumkid/usercentral/ActivityNotificationAPITests.java
+++ src/test/java/com/jumkid/usercentral/ActivityNotificationAPITests.java
@@ -2,9 +2,9 @@ package com.jumkid.usercentral;
 
 import com.jumkid.usercentral.model.mapper.ActivityNotificationMapper;
 import com.jumkid.usercentral.service.UserService;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +12,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.empty;
@@ -23,11 +22,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {UserCentralApplication.class, H2JpaConfig.class})
 @AutoConfigureMockMvc
 @EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:10092", "port=10092" })
-public class ActivityNotificationAPITests {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ActivityNotificationAPITests {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -36,20 +35,15 @@ public class ActivityNotificationAPITests {
 	@Autowired
 	private ActivityNotificationMapper activityNotificationMapper;
 
-	private static boolean isSetup = false;
-
-	@Before
-	public void setup() {
-		if (!isSetup) {
-			userService.saveAll(APITestsSetup.buildActivityNotificationEntities());
-			isSetup = true;
-		}
+	@BeforeAll
+	void setup() {
+		userService.saveAll(APITestsSetup.buildActivityNotificationEntities());
 	}
 
 	@Test
 	@Order(1)
 	@WithMockUser(username="guest", password="guest", authorities="GUEST_ROLE")
-	public void whenGuestAccess_shouldGet401Forbidden() throws Exception {
+	void whenGuestAccess_shouldGet401Forbidden() throws Exception {
 		mockMvc.perform(get("/user/activityNotifications")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isForbidden());
@@ -58,7 +52,7 @@ public class ActivityNotificationAPITests {
 	@Test
 	@Order(2)
 	@WithMockUser(username="test", password="test", authorities="USER_ROLE")
-	public void whenUserCall_shouldGetActivities() throws Exception {
+	void whenUserCall_shouldGetActivities() throws Exception {
 		mockMvc.perform(get("/user/activityNotifications")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
@@ -68,7 +62,7 @@ public class ActivityNotificationAPITests {
 	@Test
 	@Order(3)
 	@WithMockUser(username="test", password="test", authorities="USER_ROLE")
-	public void whenGivenId_shouldGetActivityByUser() throws Exception {
+	void whenGivenId_shouldGetActivityByUser() throws Exception {
 		mockMvc.perform(get("/user/activityNotifications/2")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
@@ -78,7 +72,7 @@ public class ActivityNotificationAPITests {
 	@Test
 	@Order(4)
 	@WithMockUser(username="guest", password="guest", authorities="GUEST_ROLE")
-	public void whenNotOwnerGetActivity_shouldGet403Forbidden() throws Exception {
+	void whenNotOwnerGetActivity_shouldGet403Forbidden() throws Exception {
 		mockMvc.perform(get("/user/activityNotifications/1")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isForbidden());
@@ -87,7 +81,7 @@ public class ActivityNotificationAPITests {
 	@Test
 	@Order(5)
 	@WithMockUser(username="test", password="test", authorities="USER_ROLE")
-	public void whenGivenId_ShouldUpdateUnreadFlag() throws Exception {
+	void whenGivenId_ShouldUpdateUnreadFlag() throws Exception {
 		mockMvc.perform(put("/user/activityNotifications/2/unread")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isAccepted())
@@ -97,7 +91,7 @@ public class ActivityNotificationAPITests {
 	@Test
 	@Order(6)
 	@WithMockUser(username="test", password="test", authorities="USER_ROLE")
-	public void whenGivenId_ShouldDeleteSuccessfully() throws Exception {
+	void whenGivenId_ShouldDeleteSuccessfully() throws Exception {
 		mockMvc.perform(delete("/user/activityNotifications/1")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNoContent())
